83b461f1678e0db63d278058ecc955eb704d8083
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-12-24 18:21:22 +0530

    created initial files

---
+++ collections/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+    var price string = "$48.95"
+
+    var currency byte = price[0]
+    var amountString string = price[1:]
+    amount, parseErr  := strconv.ParseFloat(amountString, 64)
+
+    fmt.Println("Currency:", currency)
+    if (parseErr == nil) {
+        fmt.Println("Amount:", amount)
+    } else {
+        fmt.Println("Parse Error:", parseErr)
+    }
+}
